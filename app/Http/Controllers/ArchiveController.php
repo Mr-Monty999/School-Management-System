85e9bab079770896258da9945103672bf69305ea
ISNewton <ashrfalhag.11@gmail.com>
2022-08-08 21:09:02 +0200

    Provide search by real name instead of username in archive

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-<<<<<<< HEAD
-=======
+use Nette\Utils\Json;
 use App\Models\Employe;
 use App\Models\Parents;
 use App\Models\Student;
 use App\Models\Teacher;
 use Illuminate\Http\Request;
->>>>>>> 31d45a712c422ede1a77c940ac1bbeb06cdbfcbb
-use App\Services\FileUploadService;
 use App\Services\JsonService;
-use Nette\Utils\Json;
+use App\Services\FileUploadService;
 
 class ArchiveController extends Controller
 {
@@ -42,25 +39,17 @@ class ArchiveController extends Controller
     public function search($pageNumber, $name)
     {
         $users = User::with("employe", "teacher", "student")
-<<<<<<< HEAD
-        ->whereHas('employe',function($q) use($name) {
-            $q->where('employe_name','LIKE',"%$name%")->onlyTrashed();
-        })
-        ->orWhereHas('teacher',function($q) use($name) {
-            $q->where('teacher_name','LIKE',"%$name%")->onlyTrashed();
-        })
-        ->orWhereHas('student',function($q) use($name) {
-            $q->where('student_name','LIKE',"%$name%")->onlyTrashed();
-        })
-        ->onlyTrashed()
-        ->paginate(5);
-=======
+            ->whereHas('employe', function ($q) use ($name) {
+                $q->where('employe_name', 'LIKE', "%$name%")->onlyTrashed();
+            })
+            ->orWhereHas('teacher', function ($q) use ($name) {
+                $q->where('teacher_name', 'LIKE', "%$name%")->onlyTrashed();
+            })
+            ->orWhereHas('student', function ($q) use ($name) {
+                $q->where('student_name', 'LIKE', "%$name%")->onlyTrashed();
+            })
             ->onlyTrashed()
-            ->where("username", "LIKE", "%$name%")
-            ->paginate(5, ['*'], 'page', $pageNumber);
-
-        //$users = User::with("employe", "teacher", "student")->where("username", "LIKE", "%$name%")->onlyTrashed()->paginate(5);
->>>>>>> 31d45a712c422ede1a77c940ac1bbeb06cdbfcbb
+            ->paginate(5);
 
         return view('archive.table', compact('users'));
     }
@@ -74,7 +63,7 @@ class ArchiveController extends Controller
         return JsonService::responseSuccess("تم الإستعادة بنجاح", $user);
     }
 
-    public function restoreAll(Request $request)
+    public function restoreAll()
     {
         User::onlyTrashed()->restore();
         Employe::onlyTrashed()->restore();
@@ -86,7 +75,7 @@ class ArchiveController extends Controller
         return JsonService::responseSuccess("تم الإسترجاع بنجاح", null);
     }
 
-    public function destroyAll(Request $request)
+    public function destroyAll()
     {
         User::onlyTrashed()->forceDelete();
         return JsonService::responseSuccess("تم الحذف بنجاح", null);
@@ -95,36 +84,8 @@ class ArchiveController extends Controller
     public function destroy(User $user)
     {
         $type = $user->type;
-        FileUploadService::deleteImage(public_path($user->$type->$type.'_photo'));
+        FileUploadService::deleteImage(public_path($user->$type->$type . '_photo'));
 
-<<<<<<< HEAD
-=======
-            FileUploadService::deleteImage(public_path($user->teacher->teacher_photo));
-        } elseif ($user->type == 'student') {
-
-            FileUploadService::deleteImage(public_path($user->student->student_photo));
-        }
-
-
-
-
-
-        /*
-        Note:
-
-        When You Delete User Parmently, The Related Data Also Will Be Deleted Auto
-
-        */
-
-
-        /* if ($user->hasRole("employe") == 'employe')
-            FileUploadService::deleteImage(public_path($user->employe()->onlyTrashed()->first()->employe_photo));
-        elseif ($user->hasRole("teacher"))
-            FileUploadService::deleteImage(public_path($user->teacher()->onlyTrashed()->first()->teacher_photo));
-        else
-            FileUploadService::deleteImage(public_path($user->student()->onlyTrashed()->first()->student_photo));
- */
->>>>>>> 31d45a712c422ede1a77c940ac1bbeb06cdbfcbb
         $user->forceDelete();
         return JsonService::responseSuccess("تم الحذف بنجاح", $user);
     }
